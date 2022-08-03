63f58bd1015857fa904d0bafa3f4b6e60dd2f93f
Stephanie Menezes Cardozo <71109139+stecardoso@users.noreply.github.com>
2022-08-03 08:26:13 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,55 @@
+function ajax_get(url, callback) {
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+		if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+			console.log("responseText:" + xmlhttp.responseText);
+			try {
+				var data = JSON.parse(xmlhttp.responseText);
+			} catch (err) {
+				console.log(err.message + " in " + xmlhttp.responseText);
+				return;
+			}
+			callback(data);
+		}
+	};
+
+	xmlhttp.open("GET", url, true);
+	xmlhttp.send();
+}
+
+ajax_get(
+	"https://api.thecatapi.com/v1/images/search?size=small",
+	function(data) {
+		var html = '<img src="' + data[0]["url"] + '">';
+		document.getElementById("image").innerHTML = html;
+	}
+);
+
+function getImage() {
+	function ajax_get(url, callback) {
+		var xmlhttp = new XMLHttpRequest();
+		xmlhttp.onreadystatechange = function() {
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				console.log("responseText:" + xmlhttp.responseText);
+				try {
+					var data = JSON.parse(xmlhttp.responseText);
+				} catch (err) {
+					console.log(err.message + " in " + xmlhttp.responseText);
+					return;
+				}
+				callback(data);
+			}
+		};
+
+		xmlhttp.open("GET", url, true);
+		xmlhttp.send();
+	}
+
+	ajax_get(
+		"https://api.thecatapi.com/v1/images/search?size=small",
+		function(data) {
+			var html = '<img src="' + data[0]["url"] + '">';
+			document.getElementById("image").innerHTML = html;
+		}
+	);
+}
